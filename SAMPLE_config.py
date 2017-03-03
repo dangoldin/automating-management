b7c16ea45a1b9a4c860134420c3f60664c6893ce
Dan Goldin <dangoldin@gmail.com>
2017-03-02 23:13:47 -0500

    Very quick v1

---
+++ SAMPLE_config.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python
+
+# Google sheets settings
+WORKBOOK = 'Calendars'
+WORKSHEET = 'On Call Cal'
+
+# Slack settings
+SLACK_TOKEN = ''
+SQUADS = ['Backend', 'Frontend']
+
---
+++ post_schedule.py
@@ -0,0 +1,45 @@
+#! /usr/bin/env python
+
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+from slackclient import SlackClient
+
+import config
+
+sc = SlackClient(config.SLACK_TOKEN)
+
+scope = ['https://spreadsheets.google.com/feeds']
+
+credentials = ServiceAccountCredentials.from_json_keyfile_name('credentials.json', scope)
+
+gc = gspread.authorize(credentials)
+
+wkbook = gc.open(config.WORKBOOK)
+
+wks = wkbook.worksheet(config.WORKSHEET)
+
+rows = wks.get_all_values()
+
+squads = config.SQUADS
+
+users = sc.api_call('users.list')
+users = users['members']
+
+user_map = {}
+for user in users:
+  user_map[user['profile']['real_name'].lower()] = user['name'].lower()
+
+msg = ''
+header = rows[0]
+for row in rows[1:]:
+  rowmap = dict(zip(header, row))
+
+  if rowmap['Current'] == 'Yes':
+    for squad in squads:
+      msg += squad + ': ' + rowmap[squad] + ' @' + user_map[rowmap[squad].lower()] + '\n'
+
+sc.api_call(
+  "chat.postMessage",
+  channel="#tmp-slack-api",
+  text=msg
+)
