697be4f896c14bf07af28c7894b93f33bdb28466
Dan Goldin <dgoldin@triplelift.com>
2017-12-03 22:40:16 -0500

    Lots of cleanup

--- jira-fun.py
+++ jira-fun.py
@@ -1,86 +1,94 @@
 #! /usr/bin/env python
 
+import sys
 from collections import Counter, defaultdict
 from jira import JIRA
+from math import ceil
 
 import config
 
-jira = JIRA(config.JIRA_URL, basic_auth=(config.JIRA_USERNAME, config.JIRA_PASSWORD))
+from util import print_dict, get_or_float_zero
 
-def get_custom_field_key(name):
-    all_fields = jira.fields()
-    found_field = None
-    for field in all_fields:
-        if field['name'] == name:
-            return field['key']
-    return found_field
+class JiraAnalysis():
+    def __init__(self):
+        self.jira = JIRA(config.JIRA_URL, basic_auth=(config.JIRA_USERNAME, config.JIRA_PASSWORD))
+        self.sprint_field = self.get_custom_field_key('Sprint')
+        self.story_point_field = self.get_custom_field_key('Story Points')
 
-def get_squad(issue):
-    for label in issue.fields.labels:
-        for squad_label in config.SQUAD_LABELS:
-            if squad_label.lower() in label.lower():
-                return squad_label
-    return None
+    def get_custom_field_key(self, name):
+        all_fields = self.jira.fields()
+        found_field = None
+        for field in all_fields:
+            if field['name'] == name:
+                return field['key']
+        return found_field
 
-def get_priority_stats(issues, story_point_field):
-    priority_count = Counter()
-    priority_story_points = defaultdict(float)
-    no_priority_stories = []
-    for issue in issues:
+    def get_squad(self, issue):
         for label in issue.fields.labels:
-            if 'priority:' in label:
-                priority = int(label.split(':')[1])
-                try:
-                    story_points = float(getattr(issue.fields, story_point_field))
-                except:
-                    story_points = 0.0
-                priority_count.update([priority])
-                priority_story_points[priority] += float(story_points)
-            else:
-                no_priority_stories.append(issue)
-    return priority_count, priority_story_points, no_priority_stories
+            for squad_label in config.SQUAD_LABELS:
+                if squad_label.lower() in label.lower():
+                    return squad_label
+        return None
 
-def print_dict(d):
-    for key, val in d.iteritems():
-        print "\t", key, val
-
-def analyze_priorities():
-    story_point_field = get_custom_field_key('Story Points')
-    priorities = ['priority:%d' % i for i in range(1,26)]
-    issues = jira.search_issues('status = Done and resolutiondate >= "2017-11-01" and resolutionDate < "2017-12-01" AND type = story AND labels in (' + ','.join(priorities) + ')', maxResults=1000)
-
-    priority_count, priority_story_points, no_priority_stories = get_priority_stats(issues, story_point_field)
-
-    print 'Priority counts'
-    print_dict(priority_count)
-
-    print 'Priority story points'
-    print_dict(priority_story_points)
-
-    print 'No priorities'
-    for issue in no_priority_stories:
-        print "\t", issue, issue.fields.summary
-
-def analyze_sprint_lag(sprint_field, story_point_field):
-    # Very ugly, will clean up
-    MAX_RESULTS = 100
-    squad_sprint_counts = defaultdict(list)
-    squad_sprint_story_point_sum = defaultdict(float)
-    squad_story_point_sum = defaultdict(float)
-    issues = jira.search_issues('status = Done and resolutiondate >= "2017-10-01" and resolutionDate < "2017-12-01" AND type = story', maxResults=MAX_RESULTS)
-    total = issues.total
-    for page in range(1+total/MAX_RESULTS):
-        # Repeat for now, clean this up later so pagination actually works better
-        print 'Getting page', page
-        issues = jira.search_issues('status = Done and resolutiondate >= "2017-10-01" and resolutionDate < "2017-12-01" AND type = story', maxResults=MAX_RESULTS, startAt=page * MAX_RESULTS)
-        print 'Retrieved', len(issues)
+    def get_priority_stats(self, issues):
+        priority_count = Counter()
+        priority_story_points = defaultdict(float)
+        no_priority_stories = []
         for issue in issues:
-            squad = get_squad(issue)
-            num_sprints = len(getattr(issue.fields, sprint_field))
-            story_points = getattr(issue.fields, story_point_field, 0.0)
-            if story_points is None:
-                story_points = 0.0
-            print issue, squad, issue.fields.summary, num_sprints, story_points
+            had_label = False
+            for label in issue.fields.labels:
+                if 'priority:' in label:
+                    priority = int(label.split(':')[1])
+                    story_points = get_or_float_zero(issue.fields, self.story_point_field)
+                    priority_count.update([priority])
+                    priority_story_points[priority] += float(story_points)
+                    had_label = True
+            if not had_label:
+                no_priority_stories.append(issue)
+        return priority_count, priority_story_points, no_priority_stories
+
+    # Don't force user to paginate
+    def get_issues(self, query):
+        all_issues = []
+        MAX_RESULTS = 100
+        issues = self.jira.search_issues(query, maxResults=MAX_RESULTS)
+        total = issues.total
+        all_issues.extend(list(issues))
+        if total > MAX_RESULTS: # Actually need to paginate
+            for page in range(1, int(ceil(1.0*total/MAX_RESULTS))):
+                print 'Getting page', page
+                issues = self.jira.search_issues(query, maxResults=MAX_RESULTS, startAt=page * MAX_RESULTS)
+                print 'Retrieved', len(issues)
+                all_issues.extend(list(issues))
+        print 'Total retrieved', len(all_issues)
+        return all_issues
+
+    def analyze_priorities(self, start_date, end_date):
+        # priorities = ['priority:%d' % i for i in range(1,26)]
+        # issues = self.get_issues('status = Done and resolutiondate >= "' + start_date + '" and resolutionDate < "' + end_date + '" AND type = story AND labels in (' + ','.join(priorities) + ')')
+        issues = self.get_issues('status = Done and resolutiondate >= "' + start_date + '" and resolutionDate < "' + end_date + '" AND type = story')
+        priority_count, priority_story_points, no_priority_stories = self.get_priority_stats(issues)
+
+        print 'Priority counts'
+        print_dict(priority_count)
+
+        print 'Priority story points'
+        print_dict(priority_story_points)
+
+        print 'No priorities'
+        for issue in no_priority_stories:
+            print "\t", issue, issue.fields.summary
+
+    def analyze_sprint_lag(self, start_date, end_date):
+        squad_sprint_counts = defaultdict(list)
+        squad_sprint_story_point_sum = defaultdict(float)
+        squad_story_point_sum = defaultdict(float)
+        issues = self.get_issues('status = Done and resolutiondate >= "' + start_date + '" and resolutionDate < "' + end_date + '" AND type = story')
+        for issue in issues:
+            squad = self.get_squad(issue)
+            num_sprints = len(getattr(issue.fields, self.sprint_field))
+            story_points = getattr(issue.fields, self.story_point_field, get_or_float_zero)
+            # print issue, squad, issue.fields.summary, num_sprints, story_points
 
             # Has a squad and was actually done via sprint process
             if squad and num_sprints > 0:
@@ -90,10 +98,17 @@ def analyze_sprint_lag(sprint_field, story_point_field):
                     squad_sprint_story_point_sum[squad] += num_sprints * story_points
                     squad_story_point_sum[squad] += story_points
 
-    for squad, counts in squad_sprint_counts.iteritems():
-        print squad, sum(counts)*1.0/len(counts), squad_sprint_story_point_sum[squad]/squad_story_point_sum[squad]
+        for squad, counts in squad_sprint_counts.iteritems():
+            print squad, sum(counts)*1.0/len(counts), squad_sprint_story_point_sum[squad]/squad_story_point_sum[squad]
 
 if __name__ == '__main__':
-    sprint_field = get_custom_field_key('Sprint')
-    story_point_field = get_custom_field_key('Story Points')
-    analyze_sprint_lag(sprint_field, story_point_field)
+    start_date = sys.argv[1]
+    end_date = sys.argv[2]
+
+    ja = JiraAnalysis()
+
+    print 'Priority analysis'
+    ja.analyze_priorities(start_date, end_date)
+
+    print 'Sprint lag analysis'
+    ja.analyze_sprint_lag(start_date, end_date)
---
+++ util.py
@@ -0,0 +1,11 @@
+def print_dict(d):
+    for key, val in d.iteritems():
+        print "\t", key, val
+
+def get_or_float_zero(obj, key):
+    val = None
+    if hasattr(obj, key):
+        val = getattr(obj, key)
+    if val is None:
+        return 0.0
+    return float(val)
