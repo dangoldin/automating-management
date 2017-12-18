026a0d1c279695fb677e7a8fa418cb0d9115f2bf
Dan Goldin <dgoldin@triplelift.com>
2017-12-17 21:13:27 -0800

    Add comments

--- util.py
+++ util.py
@@ -1,13 +1,17 @@
 #! /usr/bin/env python
 
-def print_dict(d):
+# Iterate through key/vals in dictionary and print
+def print_dict(d, delim = '\t'):
     for key, val in d.iteritems():
-        print "\t", key, val
+        print delim, key, val
 
+# Get key from object and return 0 if missing or None
+# Convert to float
 def get_or_float_zero(obj, key):
     val = None
     if hasattr(obj, key):
         val = getattr(obj, key)
+    # Found as None or not found at all => 0.0
     if val is None:
         return 0.0
     return float(val)
