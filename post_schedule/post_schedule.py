f70ff3bfd2c802debd24deaa9362e4fe8ac708ff
Dan Goldin <dgoldin@triplelift.com>
2018-01-01 22:25:55 -0500

    Redo structure. Still need to fix Docker

--- post_schedule/post_schedule.py
+++ post_schedule/post_schedule.py
@@ -5,8 +5,11 @@ import sys
 import logging
 from datetime import datetime
 
-from slack_helper import SlackHelper
-from sheet_helper import GSheetHelper
+# Add parent folder so we can reference util
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from util.slack_helper import SlackHelper
+from util.sheet_helper import GSheetHelper
 
 FORMAT = '%(asctime)-15s %(message)s'
 logging.basicConfig(format=FORMAT, level=logging.DEBUG)
